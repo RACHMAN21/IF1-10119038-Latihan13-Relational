81145899a099c7759397ee08819d678dcb1e9106
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 16:20:44 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan13/relational/IF110119038Latihan13Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan13.relational;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : Relational
+ * 
+ */
+
+public class IF110119038Latihan13Relational {
+    
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a" + (bil2 >= bil1));
+        System.out.println("b <= a" + (bil2 <= bil1));
+    }
+}
